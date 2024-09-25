58b8e65172b831153be8f1101e0805ca11389993
inocando04 <152863951+inocando04@users.noreply.github.com>
2024-09-24 22:19:52 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -15,13 +15,3 @@ window.onscroll = () => {
         };
     });
 };
-
-
-var burgerMenu = document.getElementById('burger-menu');
-
-var overlay = document.getElementById('menu');
-
-burgerMenu.addEventListener('click', function() {
-  this.classList.toggle("close");
-  overlay.classList.toggle("overlay");
-});
